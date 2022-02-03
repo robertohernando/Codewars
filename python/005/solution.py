7d6d80b5fb88f7515bb77dd3e024a7c0c7ad7d35
Roberto Hernando <roberto@rhernando.net>
2022-02-03 13:42:00 +0100

    Python-java
    Separo los directorios python y java para que me sea más fácil lanzar los tests desde vsc

---
+++ python/005/solution.py
@@ -0,0 +1,10 @@
+#!python3
+def solution(number):
+    sum = 0
+    if (number > 0):
+        for i in range(3, number):
+            if (i%3 == 0) or (i%5 == 0):
+                sum += i
+    return sum
+        
+print(solution(10))        
